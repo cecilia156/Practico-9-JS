5bee3bd7ad86141d3e78aea1335601fdebdbc267
cecilia156 <115896420+cecilia156@users.noreply.github.com>
2022-11-24 20:48:40 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+function cambiarfondo1() { document.body.style.backgroundColor = "navy";
+                           document.body.style.backgroundImage = "none";
+                           document.getElementById("listas").style.color = "white";
+                            document.getElementById("titulo").style.color = "white";
+                            document.getElementById("subtitulo").style.color = "yellow";
+                            document.getElementById("tituloingredientes").style.color = "yellow";
+                            document.getElementById("titulopreparacion").style.color = "yellow";
+}
+
+ function infoprogramador() {
+                              alert("Página realizada por: Cecilia Cabral, Santa Fe, Rca. Argentina")
+}
